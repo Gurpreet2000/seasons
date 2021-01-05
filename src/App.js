52589faedaf1982aa12dc509aab079528db2e84d
Punisher <singhgurpreet1212@gmail.com>
2021-01-06 03:16:03 +0530

    use SeasonDisplay display elements acc. to season

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -15,7 +15,7 @@ class App extends React.Component {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Seaso Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "summer" ? "Let's hit the beach!" : "Burr, it's chilly";
+  const icon = season === "summer" ? "sun" : "snowflake";
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
